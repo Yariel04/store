901ec9b860721d01c36cd065ab10803258b57c54
Yariel04 <yarielgramos@gmail.com>
2024-09-13 00:58:34 +0200

    Cambios 13/09/24

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
-const productos = [
-    {
-        productos : "",
-        piezas : "",
-        talla : "",
-        precio : "",
-        img : "",
-    }
-];
+import {productos,informacion,plantilla} from "/data.js"
 
+const headers = document.querySelector(".header")
+if(headers){
+    plantilla.header({
+        father : headers,
+    })
+}
 const contenido_producto = document.querySelector(".contenido-producto")
 if(contenido_producto){
     Object.keys(productos).forEach((each_prod,index)=>{
         let res_prod = productos[each_prod];
-        var div = document.createElement('div')
-            div.setAttribute("class",`seccion-producto${index}`)
+        plantilla.productos({
+            father : contenido_producto,
+            productos : res_prod.productos,
+            piezas : res_prod.piezas,
+            talla : res_prod.talla,
+            precio : res_prod.precio,
+            img : res_prod.img,
+            identfier : index,
+        })
+    })
+}
 
-        var seccion = document.createElement("div")
-            seccion.setAttribute("class",`seccion-incluida`)
-            div.appendChild(seccion);
-
-        var div_img = document.createElement("div")
-            div_img.setAttribute("class",`imagen-incluida`)
-            seccion.appendChild(div_img)
-        var img = document.createElement("img")
-            img.setAttribute("src",`${res_prod.img}`)
-            div_img.appendChild(img)
-        
-        var div_lista = document.createElement('div')
-            div_lista.setAttribute("class","lista-incluida")
-            div_lista.innerHTML = 
-            `
-                <ul>
-                    <li><b>Producto:</b>${res_prod.productos}</li>
-                    <!--<li><b>Pieza:</b>${res_prod.piezas}</li>
-                    <li><b>Talla:</b>${res_prod.talla}</li>-->
-                    <li><b>Precio:</b>${res_prod.precio}</li>
-                </ul>
-            `
-        seccion.appendChild(div_lista)
-        contenido_producto.appendChild(div)
+const footer = document.querySelector('.footer')
+if(footer){
+    plantilla.footer({
+        father : footer,
+        tel_movil : informacion.contacto.tel_movil,
+        ubicacion : informacion.contacto.ubicacion,
+        servicio_domicilio : informacion.contacto.servicio_domicilio,
     })
 }
